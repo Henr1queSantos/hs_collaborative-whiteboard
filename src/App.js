a1bb52f4f57dc7524405c3494de18f5e90bee24c
Henr1queSantos <43189446+Henr1queSantos@users.noreply.github.com>
2026-06-26 12:30:29 +0100

    env

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { io } from 'socket.io-client';
 
 import './App.css';
 
-const socket = io('https://hs-collaborative-whiteboard-backend.onrender.com'); 
+const socket = io(process.env.REACT_APP_SOCKET_URL || 'https://hs-collaborative-whiteboard-backend.onrender.com'); 
 
 function App() {
   
